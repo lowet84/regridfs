1a4ad04f15e1fb850ae745c496b9ca99e76b5a3c
Fredrik Löwenhamn <lowet84@gmail.com>
2017-10-10 14:37:37 +0200

    Wip

--- common.js
+++ common.js
@@ -47,7 +47,7 @@ let getNextINode = async function () {
 }
 
 let addRootIfNeeded = async function () {
-  var exists = r.db(databaseName).table(nodeTable).get(0).run()
+  var exists = await r.db(databaseName).table(nodeTable).get(0).run()
   if (exists.id === undefined) {
     console.log('creating root folder')
     await r.db(databaseName).table(nodeTable).insert({
