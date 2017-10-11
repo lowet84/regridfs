aac64034b2c7ee1083f438e64e484cd388c0cb25
Fredrik Löwenhamn <lowet84@gmail.com>
2017-10-11 17:40:21 +0200

    Wip

--- regridfs.js
+++ regridfs.js
@@ -49,6 +49,10 @@ class RegridFS extends fusejs.FileSystem {
     common.debug('write', [a, b, c, d, e, f, g, h])
   }
 
+  async mknod (a, b, c, d, e, f, g, h) {
+    common.debug('mknod', [a, b, c, d, e, f, g, h])
+  }
+
   // Non-handled
   release (context, inode, fileInfo, reply) {
     reply.err(0);
