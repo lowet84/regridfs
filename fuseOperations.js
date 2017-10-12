7b44cc1d896abb203e9aca6fb4bba2c15841854f
Fredrik Löwenhamn <lowet84@gmail.com>
2017-10-12 11:13:04 +0200

    Wip

--- fuseOperations.js
+++ fuseOperations.js
@@ -89,7 +89,7 @@ let create = async function (context, inode, filename, mode, fileInfo, reply) {
   let attr = await common.getNodeAttr(result)
   var entry = await getEntry(result.id,attr)
   inspect(reply)
-  reply.none()
+  reply.open(fileInfo);
   // await reply.entry(entry)
 }
 
