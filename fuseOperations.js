d9b6efa2b3bf1f2e0ad9aad68cd99a5fe4188d1d
Fredrik Löwenhamn <lowet84@gmail.com>
2017-10-12 13:26:21 +0200

    Wip

--- fuseOperations.js
+++ fuseOperations.js
@@ -92,6 +92,7 @@ let create = async function (context, inode, filename, mode, fileInfo, reply) {
   inspect(entry)
   //reply.create(entry)
   await reply.entry(entry)
+  await reply.open(fileInfo)
 }
 
 let inspect = async function (obj) {
